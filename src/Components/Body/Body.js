a20ec6b75f25b7b9f0a733b29d28ee34f47ff268
Om Dubey <om_d@pursuitsoftware.com>
2024-07-12 11:31:08 +0530

    Setting up project

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,12 @@
+import './Body.css'
+import Header from '../Header/Header'
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <Header />
+        </div>
+    )
+}
+
+export default Body
